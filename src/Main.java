d601fbb7925c7ad27a0258e8e59e1be5998e4a7f
parisa75 <p.hoseyni75@gmail.com>
2021-01-22 10:54:41 -0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] arr = { {4, 9, 2 }, { 3, 5, 7 },
+                { 8, 1, 5 } };
+        System.out.println(MagicSquare.findMinimum(arr));
+    }
+}
